578afe44b6df786d093561e206b0229e08cbed92
Denis Onder <denis_onder@protonmail.com>
2020-03-22 12:25:14 +0100

    First profile controller test

--- src/tests/Profile.test.ts
+++ src/tests/Profile.test.ts
@@ -5,35 +5,65 @@ import apiTester from "../utils/apiTester";
 let token;
 
 const testingAccount = {
-  first_name: "Test",
-  last_name: "Account",
-  email: "test_account@example.com",
+  first_name: "Denis",
+  last_name: "Onder",
+  email: "test_user@example.com",
   password: "test12345",
   confirm_password: "test12345"
 };
 
-const editedAccount = {
-  first_name: "Edited",
-  last_name: "Account",
-  email: "edited_account@example.com",
-  password: "12345test",
-  confirm_password: "12345test"
+const testProfile = {
+  website: "https://denisonder.xyz/",
+  github: "https://github.com/denis-onder/",
+  linkedin: "https://linkedin.com/denis-onder",
+  dev: "https://dev.to/denisonder",
+  stackoverflow: "https://stackoverflow.com/users/11813625/denis-onder",
+  biography: "eternal coding noob"
 };
 
 describe("Profile controller", () => {
+  // Create a test-case user, then log the user in
+  before(async done => {
+    await apiTester("post", "/api/auth/register", testingAccount);
+    const { data } = await apiTester("post", "/api/auth/login", testingAccount);
+    token = data.token;
+    done();
+  });
   describe("Create profile", () => {
-    it("should return the profile object", async () => {});
+    it("should return the profile object", async () => {
+      const res = await apiTester(
+        "post",
+        "/api/profile/create",
+        testProfile,
+        token
+      );
+      expect(res.data).to.include.all.keys(
+        "_id",
+        "website",
+        "github",
+        "linkedin",
+        "dev",
+        "stackoverflow",
+        "biography",
+        "followers",
+        "handle",
+        "createdAt",
+        "__v"
+      );
+    });
   });
-  describe("Get profile using handle", () => {
-    it("should return the profile object", async () => {});
-  });
-  describe("Edit profile", () => {
-    it("should return the edited profile object", async () => {});
-  });
-  describe("Follow/Unfollow profile", () => {
-    it("should return the profile that is being followed/unfollowed", async () => {});
-  });
-  describe("Delete", () => {
-    it("should return an object with the deleted and timestamp props", async () => {});
-  });
+  // describe("Get profile using handle", () => {
+  //   it("should return the profile object", async () => {});
+  // });
+  // describe("Edit profile", () => {
+  //   it("should return the edited profile object", async () => {});
+  // });
+  // describe("Follow/Unfollow profile", () => {
+  //   it("should return the profile that is being followed/unfollowed", async () => {});
+  // });
+  // describe("Delete profile", () => {
+  //   it("should return an object with the deleted and timestamp props", async () => {});
+  // });
+  // Delete the test user once done with testing
+  after(async () => await apiTester("delete", "/api/auth/delete", null, token));
 });
